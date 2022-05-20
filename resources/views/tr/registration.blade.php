762233592538b9ee0342aa5218d0ff87f2d1c43a
Bayu Ramadhani <bayu1802@gmail.com>
2022-05-20 17:39:09 +0800

    ganti tampilan registrasi

--- resources/views/tr/registration.blade.php
+++ resources/views/tr/registration.blade.php
@@ -5,20 +5,9 @@
   </head>
   <body>
     @include('tr.preloader')
+    @include('tr.header')
 
-    <div class="breadcrumbs2 py-3">
-      <div class="container">
-        <div class="row">
-          <div class="col-lg-8 offset-lg-2 col-md-12 col-12">
-            <div class="breadcrumbs-content">
-              <h1 class="page-title">Pendaftaran Akun Demo</h1>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-
-    <section class="py-5 blog-single">
+    <section class="py-5 mt-custom-5 blog-single">
       <div class="container">
         <div class="row">
           <div class="col-lg-10 offset-lg-1 col-md-12 col-12">
@@ -58,32 +47,30 @@
                   ?>
                     <form method="post" action="{{ route('getRequest') }}">
                       @csrf
-                      <div class="row mb-3">
-                        <div class="col-lg-4">Nama</div>
-                        <div class="col-lg-8">
+                      <h3 class="text-center">Daftar Akun Premium Anda Gratis</h3>
+                      <!-- nama -->
+                      <div class="mb-3">
+                        <label>Nama</label>
                           <input type="text" class="form-control" name="nama" autofocus required />
-                        </div>
                       </div>
-                      <div class="row mb-3">
-                        <div class="col-lg-4">Perusahaan</div>
-                        <div class="col-lg-8">
+                      <!-- perusahaan -->
+                      <div class="mb-3">
+                        <label>Perusahaan</label>
                           <input
                             type="text"
                             class="form-control"
                             name="perusahaan"
                             required
                           />
-                        </div>
                       </div>
-                      <div class="row mb-3">
-                        <div class="col-lg-4">Email</div>
-                        <div class="col-lg-8">
+                      <!-- email -->
+                      <div class="mb-3">
+                        <label>Email</label>
                           <input type="email" class="form-control" name="email" value="<?= $email; ?>" required />
-                        </div>
                       </div>
-                      <div class="row mb-3">
-                        <div class="col-lg-4">Deskripsi Pekerjaan</div>
-                        <div class="col-lg-8">
+                      <!-- desc -->
+                      <div class="mb-3">
+                        <label>Deskripsi Pekerjaan</label>
                           <textarea
                             type="text"
                             class="form-control"
@@ -91,28 +78,20 @@
                             rows="5"
                             required
                           ></textarea>
-                        </div>
                       </div>
-                      <div class="row">
-                        <div class="col-lg-12">
-                          <button
-                            type="submit"
-                            class="btn btn-lg btn-primary float-end"
-                          >
-                            Kirim
-                          </button>
-                        </div>
+                      <!-- button -->
+                      <div class="mb-3 d-flex justify-content-end">
+                        <button
+                          type="submit"
+                          class="btn btn-lg btn-blue"
+                        >
+                          Kirim
+                        </button>
                       </div>
-                      <div class="row pt-5">
-                        <div class="col-lg-10">
-                          <hr />
-                        </div>
-                        <div class="col-lg-2">
-                          <h6 class="mt-2" style="text-align: right">
-                            Trace Report
-                          </h6>
-                        </div>
-                      </div>
+                      <!-- tr -->
+                      <h6 class="m-2">
+                        Trace Report
+                      </h6>
                     </form>
                   <?php
                     }
