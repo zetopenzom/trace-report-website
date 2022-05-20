d567895c01ef1acbbcca54126bacf53a49106917
Bayu Ramadhani <bayu1802@gmail.com>
2022-05-20 16:19:53 +0800

    update color, video, fitur

--- resources/views/tr/index.blade.php
+++ resources/views/tr/index.blade.php
@@ -63,45 +63,63 @@
     </section>
     <!-- End Hero Area -->
 
+    <!-- Start Video Area -->
+    <section id="video" class="features section">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-12">
+            <div class="d-flex justify-content-center">
+              <video width="100%" controls autoplay muted loop>
+                <source src="{{asset('video/example.mp4')}}" type="video/mp4">
+                Your browser does not support the video tag.
+              </video>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <!-- End Gallery Area -->
+
     <!-- Start Features Area -->
     <section id="features" class="features section">
       <div class="container">
         <div class="row">
           <div class="col-12">
             <div class="section-title px-3">
-              <h3 class="wow fadeInUp color-custom-gray1 fw-bolder" data-wow-delay=".4s">Our features</h3>
-              <h2 class="wow fadeInUp color-custom-gray1" data-wow-delay=".4s">
-                The various features we provide to make your  business more powerful
-              </h2>
+              <h1 class="wow fadeInUp color-custom-gray1 fw-bolder mb-3" data-wow-delay=".4s">Fitur</h1>
             </div>
           </div>
         </div>
-        <div class="row">
+        <div class="row" id="border-features"  data-wow-delay=".8s">
           <div class="col-lg-3 col-md-6 col-12 p-0">
             <!-- Start Single Feature -->
-            <div class="wow fadeInUp text-center" data-wow-delay=".2s">
-              <img src="{{asset('images/features/Absensi.png')}}" alt="#" />
+            <div class="wow fadeInUp text-center h-100" data-wow-delay=".2s" id="absensi">
+              <img src="{{asset('images/features/Absensi (1).png')}}" class="bottom p-5" alt="#" />
+              <img src="{{asset('images/features/Absensi.png')}}" class="top p-5" alt="#" />
             </div>
             <!-- End Single Feature -->
           </div>
           <div class="col-lg-3 col-md-6 col-12 p-0">
             <!-- Start Single Feature -->
-            <div class="wow fadeInUp text-center" data-wow-delay=".4s">
-              <img src="{{asset('images/features/Kolab Tim.png')}}" alt="#" />
+            <div class="wow fadeInUp text-center h-100" data-wow-delay=".4s" id="kolabtim">
+              <img src="{{asset('images/features/Kolab Tim (1).png')}}" class="bottom p-5" alt="#" />
+              <img src="{{asset('images/features/Kolab Tim.png')}}" class="top p-5" alt="#" />
             </div>
             <!-- End Single Feature -->
           </div>
           <div class="col-lg-3 col-md-6 col-12 p-0">
             <!-- Start Single Feature -->
-            <div class="wow fadeInUp text-center" data-wow-delay=".6s">
-              <img src="{{asset('images/features/Manajemen Proyek.png')}}" alt="#" />
+            <div class="wow fadeInUp text-center h-100" data-wow-delay=".6s" id="manajemenproyek">
+              <img src="{{asset('images/features/Manajemen Proyek (1).png')}}" class="bottom p-5" alt="#" />
+              <img src="{{asset('images/features/Manajemen Proyek.png')}}" class="top p-5" alt="#" />
             </div>
             <!-- End Single Feature -->
           </div>
           <div class="col-lg-3 col-md-6 col-12 p-0">
             <!-- Start Single Feature -->
-            <div class="wow fadeInUp text-center" data-wow-delay=".8s">
-              <img src="{{asset('images/features/Sales Tracking.png')}}" alt="#" />
+            <div class="wow fadeInUp text-center h-100" data-wow-delay=".8s" id="salestracking">
+              <img src="{{asset('images/features/Sales Tracking (1).png')}}" class="bottom p-5" alt="#" />
+              <img src="{{asset('images/features/Sales Tracking.png')}}" class="top p-5" alt="#" />
             </div>
             <!-- End Single Feature -->
           </div>
@@ -109,145 +127,6 @@
       </div>
     </section>
     <!-- End Features Area -->
-
-    <!-- Start Promotion Area -->
-    <section id="promo" class="w-100">
-      <div class="h-100 d-flex justify-content-center align-items-center">
-        <h1>Promotion Space</h1>
-      </div>
-    </section>
-    <!-- End Promotion Area -->
-
-    <!-- Start Achievement Area -->
-    <!-- <section class="our-achievement section">
-      <div class="container">
-        <div class="row">
-          <div class="col-lg-10 offset-lg-1 col-md-12 col-12">
-            <div class="title wow fadeInUp" data-wow-delay=".4s">
-              <h2>Aplikasi <i>Project Manajer</i> yang dibuat untuk meningkatkan produktivitas anda</h2>
-              <br><br>
-              <p>
-              Dengan satu Aplikasi, dapatkan kemudahan dalam mengelola project, komunikasi karyawan, Absensi, dan banyak fitur yang akan datang.
-              </p>
-            </div>
-          </div>
-        </div>
-      </div>
-    </section> -->
-    <!-- End Achievement Area -->
-
-    <!-- Start Pricing Table Area -->
-    <!-- <section id="pricing" class="pricing-table section">
-      <div class="container">
-        <div class="row">
-          <div class="col-12">
-            <div class="section-title px-3">
-              <h2 class="wow fadeInUp" data-wow-delay=".4s">HARGA</h2>
-              <p class="wow fadeInUp" data-wow-delay=".6s">
-              Dapatkan penawaran yang cocok untuk bisnis anda!
-              </p>
-            </div>
-          </div>
-        </div>
-        <div class="row">
-          <div class="col-lg-4">
-            <div class="d-flex justify-content-center">
-              <div class="single-table wow fadeInUp" data-wow-delay=".2s">
-                <div class="table-head text-center">
-                  <h4 class="title">STARTER</h4>
-                  <div class="price">
-                    <h2 class="amount fs-1">Rp. 100K<span class="duration">/user</span></h2>
-                  </div>
-                  <p>Paket untuk di bawah 10 karyawan</p>
-                  <div class="button mt-5">
-                    <a href="{{ route('registration') }}" class="btn bg-primary bg-gradient">Get Your Free Trial Now!</a>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div class="col-lg-4">
-            <div class="d-flex justify-content-center">
-              <div class="single-table wow fadeInUp" data-wow-delay=".2s">
-                <div class="table-head text-center">
-                  <h4 class="title fw-bolder">RECOMMENDED</h4>
-                  <div class="price">
-                    <h2 class="amount fs-1">Rp. 80K<span class="duration">/user</span></h2>
-                  </div>
-                  <p>Paket untuk 20-50 karyawan</p>
-                  <div class="button mt-5">
-                    <a href="{{ route('registration') }}" class="btn bg-primary bg-gradient">Get Your Free Trial Now!</a>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div class="col-lg-4">
-            <div class="d-flex justify-content-center">
-              <div class="single-table wow fadeInUp" data-wow-delay=".2s">
-                <div class="table-head text-center">
-                  <h4 class="title">BUSINESS</h4>
-                  <div class="price">
-                    <h2 class="amount fs-1">Rp. 65K<span class="duration">/user</span></h2>
-                  </div>
-                  <p>Paket untuk di atas 100 karyawan</p>
-                  <div class="button mt-5">
-                    <a href="{{ route('registration') }}" class="btn bg-primary bg-gradient">Get Your Free Trial Now!</a>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </section> -->
-    <!--/ End Pricing Table Area -->
-
-    <!-- Start Call To Action Area -->
-    <section class="section bg-custom-blue1 pb-5 pt-5">
-      <div class="container text-center pb-5">
-        <div class="button mb-4 wow fadeInUp" data-wow-delay=".2s">
-          <a href="{{ route('registration') }}" class="btn rounded-pill shadow fs-4" id="signin">
-              Sign in for Free
-          </a>
-        </div>
-        <p class="wow fadeInUp text-dark" data-wow-delay=".4s">
-          Dapatkan pengalaman penuh - Tanpa iklan, tanpa batasan<sup>*</sup>
-        </p>
-      </div>
-    </section>
-    <!-- End Call To Action Area -->
-
-    <!-- Start Gallery Area -->
-    <!-- <section id="video" class="features section">
-      <div class="container">
-        <div class="row">
-          <div class="col-12">
-            <div class="section-title px-3">
-              <h2 class="wow fadeInUp" data-wow-delay=".4s">
-                Galeri Kami
-              </h2>
-            </div>
-          </div>
-          <div class="col-lg-4 py-3">
-            <div class="d-flex justify-content-center">
-              <iframe width="560" height="315" src="https://www.youtube.com/embed/gMe1c4UegBY" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-            </div>
-          </div>
-          <div class="col-lg-4 py-3">
-            <div class="d-flex justify-content-center">
-              <iframe width="560" height="315" src="https://www.youtube.com/embed/NsY-9MCOIAQ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-            </div>
-          </div>
-          <div class="col-lg-4 py-3">
-            <div class="d-flex justify-content-center">
-              <iframe width="560" height="315" src="https://www.youtube.com/embed/Xmxcnf2v_gs" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-            </div>
-          </div>
-        </div>
-      </div>
-    </section> -->
-    <!-- End Gallery Area -->
 
     @include('tr.footer')
     @include('tr.script')    
--- resources/views/tr/script.blade.php
+++ resources/views/tr/script.blade.php
@@ -1,10 +1,12 @@
 <!-- ========================= JS here ========================= -->
+<script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
 <script src="{{asset('js/bootstrap.min.js')}}"></script>
 <script src="{{asset('js/wow.min.js')}}"></script>
 <script src="{{asset('js/tiny-slider.js')}}"></script>
 <script src="{{asset('js/glightbox.min.js')}}"></script>
 <script src="{{asset('js/count-up.min.js')}}"></script>
 <script src="{{asset('js/main.js')}}"></script>
+<script src="{{asset('js/custom.js')}}"></script>
 <script type="text/javascript">
     //====== counter up
     var cu = new counterUp({
@@ -15,4 +17,20 @@
     append: " ",
     });
     cu.start();
+
+    $(document).ready(function() {
+        $("#absensi").click(function() {
+            $("#absensi img.top").toggleClass("transparent");
+        });
+        $("#kolabtim").click(function() {
+            $("#kolabtim img.top").toggleClass("transparent");
+        });
+        $("#manajemenproyek").click(function() {
+            $("#manajemenproyek img.top").toggleClass("transparent");
+        });
+        $("#salestracking").click(function() {
+            $("#salestracking img.top").toggleClass("transparent");
+        });
+    });
+
 </script>
